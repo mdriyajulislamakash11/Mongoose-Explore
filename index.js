b851b759fa84cf23f6fa03c1e339d60811fb9e36
Rj Akash Chowdury <hafejmohammadakash@gmail.com>
2025-11-27 23:57:12 +0600

    21

--- index.js
+++ index.js
@@ -4,25 +4,7 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 
-//
-// 
-// 
-// 
-// 
-// 
-//
-//  
-// 
-// 
-// 
-// 
 
-// 
-// 
-// 
-// 
-// 
-// 
 
 // mongoose
 //   .connect(
